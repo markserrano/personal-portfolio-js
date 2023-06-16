716799355ad08bc418d8ad682701a7b6b0c224a1
Mark Serrano <106441738+markserrano@users.noreply.github.com>
2023-06-17 00:38:34 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/mark-hadrian-serrano/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://facebook.com/xxbyepansxx"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/imhadrian425/"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2021-2023. All Rights Reserved</p>
           </Col>
